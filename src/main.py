9b447c99262d407cb767107cb02e2ea1e4fd1987
konki sravan kumar <konkisravan@gmail.com>
2020-08-13 01:35:16 +0900

    update main.py file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-# a = 10
 print(1+3j)
 print("Hello, USTians!")
 print(type(10))
@@ -11,3 +10,34 @@ print(4/3)
 print(4%3)
 print(2.5**3)
 print(abs(-2))
+a = 4.5 + 3
+b = 5.
+print(a)
+print(a + 5.)
+print(a)
+a = a + 5.
+print(a)
+print(type(a))
+a+=2.
+a-=2.
+a/=2.
+a//=2.
+a%=2.
+a**=2.
+a = 3.+4.j
+print(a, type(a))
+a = int(5.)
+print(a, type(a))
+a = complex(5.)
+print(a, type(a))
+a = bool(5.)
+print(a, type(a))
+a = 1
+a = 0
+print(a and a, a and b, b and a, b and b)
+print(a or a, a or b, b or a, b or b)
+print(3 + 4 * 6)
+print((3 + 4) * 6)
+print((True or True) and False)
+
+
