ea7824a7ce9fe9121394d06797dc7ee45760ad29
konki sravan kumar <konkisravan@gmail.com>
2020-08-13 12:38:18 +0900

    add new lecture3 file

---
+++ src/L3_Datatypes.py
@@ -0,0 +1,89 @@
+# a = ("Good morning")
+# print(a[0])
+# print(a[3])
+# print(a[10])
+####List
+
+# a = [1,2,3,4,5]
+# b = ["sravan", "konki", "kumar", "pedda", "srunga"]
+# c = []
+# d = [13.5, 35, "sravan"]
+# e = [23, [1,2,3], "konki"]
+# print (type(a))
+# print (type(b))
+# print (type(c))
+# print (type(d))
+# print (type(e))
+
+# a = ["sravan", 'kumar', "konki", "pedda", "chinna", "akka"]
+# print (a[2])
+# print(a[2:4])
+# print(a[2:])
+# print(a[:4])
+# print(a[:])
+# print(a[-2])
+# print(len(a))
+# a.extend(["jeju", "korea"])
+# print(a)
+# a.append("ram")
+# print(a)
+# a.insert(2, "dad")
+# a.remove("ram")
+# print(a.pop())
+# print(a.index("konki"))
+# print(a.count("konki"))
+# a.sort()
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+
+####Tuple
+# a = (1,2,3,4,5)
+# b = ("sravan", "konki", "kumar", "pedda", "srunga")
+# c = ()
+# d = (13.5, 35, "sravan")
+# e = (23, (1,2,3), "konki")
+# print (type(a))
+# print (type(b))
+# print (type(c))
+# print (type(d))
+# print (type(e))
+# print(b, type(b))
+# name1, name2, name3, name4, name5 = b
+# print(name1)
+# print(len(a))
+
+
+#####Dictionary
+
+# classroom = {"sravan":20, "konki":10, "kumar":5}
+# print(classroom, type(classroom))
+# b = {}
+# print(b)
+# print(classroom["sravan"])
+# classroom["sravan"] =30
+# print (classroom)
+# classroom["ravan"] =30
+# print (classroom)
+# del(classroom["ravan"])
+# # print (classroom.keys(), classroom.values(), classroom.items())
+# k= classroom.keys()
+# print (k)
+# print ("sravan" in classroom.keys())
+# print ("srava" in classroom.keys())
+# print(30 in classroom.values())
+# print(classroom.get("sravan", "No item"))
+
+###set
+a = (1,2,3,4,5)
+b = ("sravan", "konki", "kumar", "pedda", "srunga")
+print(a, type(a))
+print(b, type(b))
+a = set(a)
+b = set(b)
+print(a, type(a))
+print(b, type(b))
+
+
+
+
