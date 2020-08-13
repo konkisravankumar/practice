6fb07571292c401373a688add6200aef59a0c997
konki sravan kumar <konkisravan@gmail.com>
2020-08-13 14:02:04 +0900

    add new lecture4 file

---
+++ src/L4_controlflow.py
@@ -0,0 +1,13 @@
+# a = 5
+# if a>0 and a>4:
+#      print("it is positive")
+#      print("it is larger than 4" )
+#
+a = 5
+if a > 0 :
+    print("it is positive")
+else:
+    print("it is negative")
+
+
+
